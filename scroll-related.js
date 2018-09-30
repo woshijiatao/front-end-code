264ce6f7c0611f140a43248fd318ac01dc192666
George Jia <george.jia@meginfo.com>
2018-09-30 10:55:32 +0800

    adjust the code

--- scroll-related.js
+++ scroll-related.js
@@ -1,7 +1,7 @@
 function hasScrolled(el, direction = "vertical") {
-    if(direction === "vertical") {
-        return el.scrollHeight > el.clientHeight;
-    }else if(direction === "horizontal") {
-        return el.scrollWidth > el.clientWidth;
-    }
- }
+  if(direction === "vertical") {
+    return el.scrollHeight > el.clientHeight;
+  } else if(direction === "horizontal") {
+    return el.scrollWidth > el.clientWidth;
+  }
+}
