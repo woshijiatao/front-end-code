178e431b91b86e288f79b0f8f1e9e03dc10733dd
George Jia <george.jia@charket.com>
2018-12-23 22:06:10 +0800

    add getRelatedTarget function

--- event-util.js
+++ event-util.js
@@ -36,5 +36,16 @@ let EventUtil = {
     } else {
       event.cancelBubble = true
     }
+  },
+  getRelatedTarget: function (event) {
+    if (event.relatedTarget) {
+      return event.relatedTarget
+    } else if (event.toElement) {
+      return event.toElement
+    } else if (event.fromElement) {
+      return event.fromElement
+    } else {
+      return null
+    }
   }
 }
